1cbfd1893e35db4feb8b54f32a687c31c88e193f
Matej <matejchalachan@gmail.com>
2023-01-21 14:26:42 +0100

    added methods to api

--- product_catalog_api/src/Controller/ProductApiController.php
+++ product_catalog_api/src/Controller/ProductApiController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,11 +16,10 @@ class ProductApiController extends AbstractController
     public function __construct(ProductRepository $productRepository)
     {
         $this->productRepository = $productRepository;
-
     }
 
-    #[Route('/api/products/', methods: ['GET'])]
-    public function getProducts()
+    #[Route('/api/products', methods: ['GET'])]
+    public function getProducts(): Response
     {
         $products = $this->productRepository->findAll();
 
@@ -29,11 +29,52 @@ class ProductApiController extends AbstractController
     }
 
     #[Route('/api/products/{id<\d+>}', methods: ['GET'])]
-    public function getProduct(int $id): Response
+    public function getProduct(Request $request): Response
     {
+        $id = $request->get('id');
 
         $product = $this->productRepository->findOne($id);
 
+        if (!$product) {
+            return $this->json('"Product with id $id not found"', 404);
+        }
+
         return $this->json($product, 200, [], ['groups' => ['read']]);
+    }
+
+    #[Route('/api/products', methods: ['POST'])]
+    public function createProduct(): Response
+    {
+
+        return $this->json(['message' => 'this is post request']);
+    }
+
+    #[Route('/api/products/{id<\d+>}', methods: ['PATCH'])]
+    public function updateProduct(Request $request): Response
+    {
+        $id = $request->get('id');
+
+        $product = $this->productRepository->findOne($id);
+
+        if (!$product) {
+            return $this->json("Product with id $id not found", 404);
+        }
+
+        $product->setName($request->get('name'));
+        $product->setPrice($request->get('price'));
+        $this->productRepository->save($product, true);
+
+        return $this->json($product, 200);
+    }
+
+    #[Route('/api/products/{id<\d+>}', methods: ['DELETE'])]
+    public function deleteProduct(): Response
+    {
+        //@TODO here should be implemented delete request
+        //@TODO remove product from cache also
+
+        /** Return 204 if successfully deleted*/
+        /** Return 404 user calls delete on previously deleted product */
+        return $this->json('Product deleted', 204);
     }
 }
